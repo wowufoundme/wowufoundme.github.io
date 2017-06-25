06c1f00e7bd80d7ca0a41a47afa9d1841e483da4
shubh <shubhkirti.work@gmail.com>
2017-06-26 00:55:28 +0530

    Major Update

--- js/script.js
+++ js/script.js
@@ -2,24 +2,35 @@ $(window).load(function(){
   $("nav").css("height", "100px");
   $("nav").css("background-color", "transparent");
   $("nav").css("border-bottom", "1px solid rgba(255,255,255,0)");
-  $("nav").css("box-shadow","0 0 0 0 rgba(255,255,255,0)");
+  $("header").css("transform","translate3d(0,0,0)");
   $(".loader").fadeIn("slow");
-  $(".loader").delay(1000).fadeOut("slow");
+  $(".loader").delay(500).fadeOut("slow");
 });
 
 $(window).scroll(function() {
-    if($(window).scrollTop()>0){
+    if($(window).scrollTop()>20){
       $('div[id^="c-"]').addClass('shift');
+      $("nav").css("color", "#222");
       $("nav").css("height", "50px");
-      $("nav").css("background-color", "#2b3137");
+      $("nav").css("background-color", "#fff");
       $("nav").css("border-bottom", "1px solid rgba(255,255,255,0.1)");
-      $("nav").css("box-shadow","0 10px 10px -15px #fff");
     }
     else{
       $('div[id^="c-"]').removeClass('shift');
+      $("nav").css("color", "#fff");
       $("nav").css("height", "100px");
       $("nav").css("background-color", "transparent");
       $("nav").css("border-bottom", "1px solid rgba(255,255,255,0)");
-      $("nav").css("box-shadow","0 0 0 0 #fff");
+    }
+});
+
+$(window).scroll(function() {
+    if($(window).scrollTop()>200){
+      $("header").css("transform","translate3d(0,-500px,0)");
+      $(".list--container--main li").css("transform","translate3d(0,0,0)");
+    }
+    else{
+      $("header").css("transform","translate3d(0,0,0)");
+      $(".list--container--main li").css("transform","translate3d(200%,0,0)");
     }
 });
