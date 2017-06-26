8cb6abe64678df26b60cd4d3515f618713c2b0c4
shubhkirti <shubhkirti.work@gmail.com>
2017-06-26 22:36:47 +0530

    Major Update

--- js/script.js
+++ js/script.js
@@ -1,23 +1,21 @@
 $(window).load(function(){
+  $("nav").css("color", "#fff");
   $("nav").css("height", "100px");
   $("nav").css("background-color", "transparent");
   $("nav").css("border-bottom", "1px solid rgba(255,255,255,0)");
   $("header").css("transform","translate3d(0,0,0)");
   $(".content--wrapper").css("transform","translate3d(0,0,0)");
-  $(".loader").fadeIn("slow");
   $(".loader").delay(1000).fadeOut("slow");
 });
 
 $(window).scroll(function() {
     if($(window).scrollTop()>20){
-      $('div[id^="c-"]').addClass('shift');
       $("nav").css("color", "#222");
       $("nav").css("height", "50px");
       $("nav").css("background-color", "#fff");
       $("nav").css("border-bottom", "1px solid rgba(255,255,255,0.1)");
     }
     else{
-      $('div[id^="c-"]').removeClass('shift');
       $("nav").css("color", "#fff");
       $("nav").css("height", "100px");
       $("nav").css("background-color", "transparent");
