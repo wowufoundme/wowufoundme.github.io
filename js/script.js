7952435484b299eeef6e3ca43e3113765686d995
shubh <shubhkirti.work@gmail.com>
2017-07-05 21:24:32 +0530

    Update

--- js/script.js
+++ js/script.js
@@ -10,13 +10,11 @@ $(window).load(function(){
 
 $(window).scroll(function() {
     if($(window).scrollTop()>20){
-      $("nav").css("color", "#222");
       $("nav").css("height", "50px");
-      $("nav").css("background-color", "#fff");
+      $("nav").css("background-color", "#000");
       $("nav").css("border-bottom", "1px solid rgba(255,255,255,0.1)");
     }
     else{
-      $("nav").css("color", "#fff");
       $("nav").css("height", "100px");
       $("nav").css("background-color", "transparent");
       $("nav").css("border-bottom", "1px solid rgba(255,255,255,0)");
@@ -25,12 +23,10 @@ $(window).scroll(function() {
 
 $(window).scroll(function() {
     if($(window).scrollTop()>50){
-      $("body").css("background-color","#222");
       $("header").css("transform","translate3d(0,-500px,0)");
       $(".content--wrapper").css("transform","translate3d(0,-100px,0)");
     }
     else{
-      $("body").css("background-color","#24292e");
       $("header").css("transform","translate3d(0,0,0)");
       $(".content--wrapper").css("transform","translate3d(0,0,0)");
     }
@@ -44,3 +40,13 @@ $(window).scroll(function() {
       $(".list--container--main li").css("transform","translate3d(200%,0,0)");
     }
 });
+
+$(document).ready(function(){
+  $('#move-down').click(function(event){
+    event.preventDefault();
+    $('html, body').animate({scrollTop: 500}, 1500);
+  });
+});
+
+var scrollValue = scrollTop();
+zz
