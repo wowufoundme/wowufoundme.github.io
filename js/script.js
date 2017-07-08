94f9dae6b8a5b2a297e2ae584940991fede02b2b
shubh <shubhkirti.work@gmail.com>
2017-07-08 19:06:19 +0530

    Update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 $(window).load(function(){
-  $("nav").css("color", "#fff");
-  $("nav").css("height", "100px");
-  $("nav").css("background-color", "transparent");
-  $("nav").css("border-bottom", "1px solid rgba(255,255,255,0)");
-  $("header").css("transform","translate3d(0,0,0)");
+  if($(window).scrollTop()>1){
+    $("nav").css("height", "50px");
+    $("nav").css("border-bottom", "1px solid #6715E0");
+  }
+  else{
+    $("nav").css("height", "100px");
+    $("nav").css("border-bottom", "1px solid rgba(255,255,255,0)");
+  }
   $(".content--wrapper").css("transform","translate3d(0,0,0)");
   $(".loader").delay(1000).fadeOut("slow");
 });
@@ -11,24 +14,11 @@ $(window).load(function(){
 $(window).scroll(function() {
     if($(window).scrollTop()>20){
       $("nav").css("height", "50px");
-      $("nav").css("background-color", "#000");
-      $("nav").css("border-bottom", "1px solid rgba(255,255,255,0.1)");
+      $("nav").css("border-bottom", "1px solid #6715E0)");
     }
     else{
       $("nav").css("height", "100px");
-      $("nav").css("background-color", "transparent");
       $("nav").css("border-bottom", "1px solid rgba(255,255,255,0)");
-    }
-});
-
-$(window).scroll(function() {
-    if($(window).scrollTop()>50){
-      $("header").css("transform","translate3d(0,-500px,0)");
-      $(".content--wrapper").css("transform","translate3d(0,-100px,0)");
-    }
-    else{
-      $("header").css("transform","translate3d(0,0,0)");
-      $(".content--wrapper").css("transform","translate3d(0,0,0)");
     }
 });
 
@@ -44,9 +34,8 @@ $(window).scroll(function() {
 $(document).ready(function(){
   $('#move-down').click(function(event){
     event.preventDefault();
-    $('html, body').animate({scrollTop: 500}, 1500);
+    $('html, body').animate({scrollTop: 600}, 500);
   });
 });
 
 var scrollValue = scrollTop();
-zz
