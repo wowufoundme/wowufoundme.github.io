33f7591393e75567ca1f5102c9dfe7bd2c11b632
wowufoundme <shubhkirti.work@gmail.com>
2018-01-17 17:05:35 +0530

    Layout Update

--- js/main.js
+++ js/main.js
@@ -3,4 +3,16 @@ $(document).ready(function(){
     $('#social-view').click(function(){
         $('[id=s-icon]').toggleClass('icon-shown');
     });
+    $('.loader').delay(1200).fadeOut(500);
+});
+
+$(document).ready(function(){
+  $('.title').css('transform','translateX(0)');
+  $('.title').css('opacity','1');
+  $('.hero-image').css('transform','translateX(0)');
+  $('.hero-image').css('opacity','1');
+  $('.nav-toggle').click(function(){
+    $('#nav').toggleClass('is-open');
+    $('#nav-toggle').toggleClass('is-toggled');
+  });
 });
