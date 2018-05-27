f0672d02bc8cef512360c8edd0bc1b63e9642538
wowufoundme <shubhkirti.work@gmail.com>
2018-05-28 02:36:14 +0530

    Changes

--- compiled/script.js
+++ compiled/script.js
@@ -12,3 +12,7 @@ $(window).on('load',function(){
   $('.cover--1').toggleClass('cover--1--removed');
   $('.cover--2').toggleClass('cover--2--removed');
 });
+
+particlesJS.load('particles-js', 'compiled/assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
