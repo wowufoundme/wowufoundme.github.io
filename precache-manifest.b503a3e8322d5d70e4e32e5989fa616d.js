5a1a94febca8a83c5e671e7b40ebcfd5e3c53dc2
Shubhkirti Sharma <shubhkirti.work@gmail.com>
2020-08-25 14:02:33 +0530

    revamp website

---
+++ precache-manifest.b503a3e8322d5d70e4e32e5989fa616d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c101ee61943fb0b93f0873e2fb538c2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8d145c5a5154dba04fa",
+    "url": "/static/css/main.dd287009.chunk.css"
+  },
+  {
+    "revision": "0a5b319457f812cbc929",
+    "url": "/static/js/2.33b34f7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.33b34f7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8d145c5a5154dba04fa",
+    "url": "/static/js/main.8bde0cc1.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "693af09fa404280d3bcc6994c99919b2",
+    "url": "/static/media/cover.693af09f.jpg"
+  },
+  {
+    "revision": "68740f73c286a981d02f212c1fe118ac",
+    "url": "/static/media/hand.68740f73.svg"
+  }
+]);
